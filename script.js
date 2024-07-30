0c927983be89f2bd8094917dfb33d7ea42e79127
naimackerman <naimackerman@gmail.com>
2024-07-30 00:40:39 +0000

    update

--- script.js
+++ script.js
@@ -52,3 +52,23 @@ $('a[href*="#"]')
       );
     }
   });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var modal = document.getElementById("pdfModal");
+  var btn = document.getElementById("openModal");
+  var span = document.getElementsByClassName("close")[0];
+
+  btn.onclick = function () {
+    modal.style.display = "block";
+  };
+
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  window.onclick = function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  };
+});
